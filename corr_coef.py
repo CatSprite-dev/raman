31be86869de3a20fb7ca267cd73fe93e378a2e1c
CatSprite-dev <catspriteee@gmail.com>
2025-12-29 16:54:43 +0300

    Added heap data structures

--- corr_coef.py
+++ corr_coef.py
@@ -4,6 +4,7 @@ import numpy as np
 import rampy as rp
 import pandas as pd
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from spectrum import Spectrum
 from process import trim, interpol
 from dotenv import load_dotenv
@@ -26,8 +27,6 @@ def validate_dataframe_of_map(df: pd.DataFrame) -> pd.DataFrame:
     df = df.rename(columns=new_columns)
     return df.dropna(axis=1, how='all')
 
-
-@timeit
 def main():
     load_dotenv()
     map_path = os.environ.get("MAP_PATH")
@@ -35,9 +34,12 @@ def main():
     df = validate_dataframe_of_map(pd.read_csv(map_path, sep=sep)) #создаем DataFrame pandas и валидируем его
     grouped = df.groupby(['X', 'Y']) #Группируем строки по уникальным значениям X и Y
     
+    print(f"\033[32m\n===| {map_path} |===\n\033[0m")
+    
     spectra = []
     coordinates = []
-    for coord, group in grouped:
+    width = len("Приведение к базовой линии")
+    for coord, group in tqdm(grouped, desc=f"{'Получение спектров':{width}}", ncols=120, bar_format='{l_bar}{bar:40}|{n_fmt}/{total_fmt}'):
         spectrum = group[['W', 'I']].values #Создаём np массив вида [[W1, I1], [W2, I2], ...]
         spectrum_sorted = spectrum[spectrum[:, 0].argsort()] #Сортируем массив
         spectra.append(spectrum_sorted)
@@ -45,7 +47,7 @@ def main():
         
     corrected_spectra = []
     baselines = []
-    for spectrum in spectra:
+    for spectrum in tqdm(spectra, desc=f"{'Приведение к базовой линии':{width}}", ncols=120, colour="blue", bar_format='{l_bar}{bar:40}|{n_fmt}/{total_fmt}'):
         x = spectrum[:, 0] #Первый столбик W
         y = spectrum[:, 1] #Второй столбик I
         correct_y, baseline = rp.baseline(x, y, method="als", niter = 5) #Приводим спектр к базовой линии 
@@ -53,14 +55,14 @@ def main():
         baselines.append(np.column_stack((x, baseline))) #То же самое и для базовой линии
     
     normalised_spectra = []
-    for spectrum in corrected_spectra:
+    for spectrum in corrected_spectra: #tqdm(corrected_spectra, desc=f"{'Нормализация спектров':{width}}", ncols=120):
         x = spectrum[:, 0] #Первый столбик W
         y = spectrum[:, 1] #Второй столбик I
         normalise_y = rp.normalise(y, x) #Нормализуем спектры, чтобы интенсивность была в пределах от 0 до 1
         normalised_spectra.append(np.column_stack((x, normalise_y))) #Создаем двумерный np массив из одномерных массивов x и normalise_y
     
     smoothed_spectra = []
-    for spectrum in normalised_spectra: #Сглаживаем спектры, используя метод smooth и проделывая те же самые процедуры, что и в циклах выше
+    for spectrum in normalised_spectra: #tqdm(normalised_spectra, desc=f"{'Сглаживание спектров':{width}}", ncols=120): #Сглаживаем спектры, используя метод smooth и проделывая те же самые процедуры, что и в циклах выше
         x = spectrum[:, 0]
         y = spectrum[:, 1]
         smooothed_y = rp.smooth(x, y, method="savgol", window_length = 11, polyorder = 5)
@@ -70,7 +72,8 @@ def main():
     library = os.listdir(library_path) #Получаем список спектров в папке
     common_count = 0
 
-    for lib_spec in library:
+    result = {}
+    for lib_spec in tqdm(library, desc=f"{'Подсчет результатов':{width}}", ncols=120, colour="red", bar_format='{l_bar}{bar:40}|{n_fmt}/{total_fmt}'):
         count = 0
         try:
             lib_spectrum = Spectrum(f"{library_path}/{lib_spec}")  # Создаем экземпляр класса Spectrum
@@ -94,7 +97,7 @@ def main():
                 name = full_name.split(".")[0]
 
             if corr_coef > 0.5:
-                if name.lower() in ["titanite.txt", "brookite.txt", "magnetite.txt"]:
+                if name.lower() in ["titanite.txt", "brookite.txt", "magnetite.txt", "ilmenite", "anatase"]:
                     if corr_coef > 0.65:
                         count += 1
                         common_count += 1
@@ -113,13 +116,12 @@ def main():
                 #plt.show()       
             """         
         if count > 0:
-            print(f"{count/len(spectra)*100:.2f} % - {count} - {name}")
+            #tqdm.write(f"{count/len(spectra)*100:.2f} % - {count} - {name}")
+            result[name] = count
     print(f"Общее количество спектров = {common_count}")
+    
+    for key, value in sorted(result.items()):
+        print(f"{key} - {value}" )
         
-        
-
-
-
-
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,10 @@
+from max_heap import *
+
+heap = MaxHeap()
+heap.push(1, "anatase")
+heap.push(2, "rutile")
+heap.push(3, "magnetite")
+heap.push(4, "diamond")
+heap.push(5, "hui")
+
+print(heap.peek())
